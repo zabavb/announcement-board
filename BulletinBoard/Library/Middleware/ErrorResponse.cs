b0804f1e5706dad7ee75aee0927608e8067e5d2b
zabavb <bilonizkavik@gmail.com>
2025-07-09 16:53:28 +0200

    Created single format of error response which is shared for server and client access.

---
+++ BulletinBoard/Library/Middleware/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Middleware;
+
+public class ErrorResponse
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public string? Details { get; set; }
+}
