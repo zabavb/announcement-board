6c3412f4abfce1961c811177e23c1337f1b3d8e3
zabavb <bilonizkavik@gmail.com>
2025-07-09 16:29:04 +0200

    Minor-fix to announcements` filtering by category.

--- BulletinBoard/AnnouncementApi/Services/Interfaces/IAnnouncementService.cs
+++ BulletinBoard/AnnouncementApi/Services/Interfaces/IAnnouncementService.cs
@@ -4,7 +4,7 @@ namespace AnnouncementApi.Services.Interfaces;
 
 public interface IAnnouncementService
 {
-    public Task<ICollection<Announcement>> GetAsync(Category? subcategory);
+    public Task<ICollection<Announcement>> GetAsync(Guid? subcategory);
     public Task CreateAsync(Announcement announcement);
     public Task UpdateAsync(Announcement announcement);
     public Task DeleteAsync(Guid id);
